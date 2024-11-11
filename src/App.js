786faf799476935b5bb5d286d140ecbd755dc10f
SeikhAbubakar5 <seikhabubakar47@gmail.com>
2024-11-11 18:04:38 +0530

    error handlings

--- src/App.js
+++ src/App.js
@@ -1,58 +1,69 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
 
-  const [fname, setFname]=useState("")
-  const [lname, setLname]=useState("")
-  const [fullName, setFullname]=useState("")
-  const [errors, setErrors] = useState({ firstName: false, lastName: false });
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [fullName, setFullname] = useState("");
+  const [errors, setErrors] = useState({});
 
-  const handleClick=(e)=>{
-    e.preventDefault()
-    const newErrors = {
-      firstName: !fname.trim(),
-      lastName: !lname.trim(),
-    };
-    setErrors(newErrors);
- 
-    if (newErrors.firstName || newErrors.lastName) {
-      setFullname(""); 
-      return;
+  const handleClick = (e) => {
+    e.preventDefault();
+    const validationErrors = validate({ fname, lname });
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      setFullname(`${fname} ${lname}`);
+    } else {
+      setFullname(''); 
     }
-    setFullname(`${fname} ${lname}`)
+  }
+
+  const validate = (values) => {
+    const err = {};
+    const regex = /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g;
+
+    if (!values.fname) {
+      err.fname = "Please fill out this field";
+    } else if (regex.test(values.fname)) {
+      err.fname = "This is not a correct input";
+    }
+
+    if (!values.lname) {
+      err.lname = "Please fill out this field";
+    } else if (regex.test(values.lname)) {
+      err.lname = "This is not a correct input";
+    }
+
+    return err;
   }
 
   return (
     <div className="App">
       <h1>Full Name Display</h1>
-         <form onSubmit={handleClick}>
-          <div>
-              <label>
-                FirstName:
-                <input type="text" value={fname} onChange={((e)=>setFname(e.target.value))}/>
-              </label>
-              {errors.firstName && (
-          <p style={{ color: 'red', margin: '5px 0' }}>Please fill out this filed</p>
-        )}
-          </div>
-          <div>
-            <label>
-              LastName:
-              <input type="text" value={lname} onChange={((e)=>setLname(e.target.value))}/>
-            </label>
-            {errors.lastName && (
-          <p style={{ color: 'red', margin: '5px 0' }}>Please fill out this filed</p>
-        )}
-          </div>
-          <button type="submit">Submit</button>
-         </form>
+      <form onSubmit={handleClick}>
+        <div>
+          <label>
+            FirstName:
+            <input type="text" value={fname} onChange={(e) => setFname(e.target.value)} />
+          </label>
+          {errors.fname && <p style={{ color: 'red', margin: '5px 0' }}>{errors.fname}</p>}
+        </div>
+        <div>
+          <label>
+            LastName:
+            <input type="text" value={lname} onChange={(e) => setLname(e.target.value)} />
+          </label>
+          {errors.lname && <p style={{ color: 'red', margin: '5px 0' }}>{errors.lname}</p>}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
 
-         <div>
-            <h2>Full Name: {fullName}</h2>
-         </div>
+      <div>
+        <h2>Full Name: {fullName}</h2>
+      </div>
     </div>
-    
   );
 }
 
